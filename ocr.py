e009764716050251c42c8da92e96c1d92abf29f5
abdseif <112360416+abdseif@users.noreply.github.com>
2025-03-26 07:10:39 +0300

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -2,5 +2,5 @@ import pytesseract
 from PIL import Image
 
 def extract_text_from_image(image: Image.Image) -> str:
-    custom_config = r'--oem 3 --psm 6'
+    custom_config = r'--oem 3 --psm 4'
     return pytesseract.image_to_string(image, config=custom_config)
